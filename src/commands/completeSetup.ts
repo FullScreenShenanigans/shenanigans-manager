ed4ccb1b3074e7d72b1fa492e4eba6dc691d956c
Josh Goldberg <josh@fullscreenmario.com>
2017-05-07 18:14:03 -0700

    Redid complete-setup to use plain symlinks
    linking and installing with npm is very slow. This instead uses fs.symlink everywhere to skip it after the first time, in gulp-shenanigans

--- src/commands/completeSetup.ts
+++ src/commands/completeSetup.ts
@@ -1,11 +1,9 @@
-import * as fs from "mz/fs";
-
 import { Command, ICommandArgs } from "../command";
+import { ensurePathExists } from "../utils";
 import { CloneRepository } from "./cloneRepository";
-import { Link } from "./link";
-import { LinkToDependencies } from "./linkToDependencies";
-import { RunGulp } from "./runGulp";
-import { RunGulpSetup } from "./runGulpSetup";
+import { Gulp } from "./gulp";
+import { GulpSetup } from "./gulpSetup";
+import { NpmInstall } from "./npmInstall";
 
 /**
  * Clones, links, installs, and builds all repositories locally.
@@ -19,20 +17,41 @@ export class CompleteSetup extends Command<ICommandArgs, void> {
     public async execute(): Promise<any> {
         this.ensureArgsExist("directory");
 
-        if (!(await fs.exists(this.args.directory))) {
-            await fs.mkdir(this.args.directory);
+        await this.subroutine(
+            CloneRepository,
+            {
+                directory: this.args.directory,
+                repository: "gulp-shenanigans"
+            } as ICommandArgs);
+
+        await this.subroutine(
+            NpmInstall,
+            {
+                directory: this.args.directory,
+                repository: "gulp-shenanigans"
+            });
+
+        await this.subroutine(
+            Gulp,
+            {
+                directory: this.args.directory,
+                repository: "gulp-shenanigans"
+            });
+
+        for (const repository of this.settings.allRepositories) {
+            await ensurePathExists(this.args.directory, repository);
         }
 
         await this.subroutineInAll(
             CloneRepository,
             {
-                ...this.args,
-                install: true
+                directory: this.args.directory,
+                link: true
             } as ICommandArgs);
 
-        await this.subroutineInAll(RunGulpSetup, this.args);
-        await this.subroutineInAll(RunGulp, this.args);
-        await this.subroutineInAll(Link, this.args);
-        await this.subroutineInAll(LinkToDependencies, this.args);
+        await this.subroutineInAll(GulpSetup, this.args);
+
+        // Todo: will need to parse the tsconfig.json order out of all these suckers
+        // await this.subroutineInAll(Gulp, this.args);
     }
 }
